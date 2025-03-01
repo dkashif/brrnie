6a1ec727d5e1462805d1e46af366a73b6524b171
bailispam <baileypham@arizona.edu>
2025-03-01 15:30:14 -0700

    set up django backend and configured PostgreSQL connection for Railway

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'TWeAtiDSVhRosOi', # i think it's a capital o, but could be a zero
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
 }
 
