6503a4ed8706cadf79b9b4598e6ef190ea046aee
dkashif <dkashif@arizona.edu>
2025-04-19 13:20:33 -0700

    adds logout button to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,19 +1,32 @@
 import "./Navbar.css";
-export default function Navbar() {
+export default function Navbar({ logout }) {
     return (
         <nav className="nav">
-        <a href="/" className="site-title">
-        Brrnie
+            <a href="/" className="site-title">
+                Brrnie
+            </a>
+            <ul>
+                <li>
+                    <a href="/home">Home</a>
+                </li>
+                <li>
+                    <a href="/inventory">Inventory</a>
+                </li>
+                {logout && (
+    <li>
+        <a
+            href="#logout"
+            className="logout-btn"
+            onClick={e => {
+                e.preventDefault();
+                logout();
+            }}
+        >
+            Logout
         </a>
-        <ul>
-            <li>
-                <a href="/home">Home</a>
-            </li>
-            <li>
-                <a href="/inventory">Inventory</a>
-            </li>
-        </ul>
+    </li>
+)}
+            </ul>
         </nav>
-    )
+    );
 }
-
